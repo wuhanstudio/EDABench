08b654f6c5fb785d0e03be38abc1bb3448261e6e
wuhanstudio <wuhanstudios@gmail.com>
2026-06-12 17:11:57 +0100

    [librelane] Add custome plugin

---
+++ librelane_plugin_ml/__init__.py
@@ -0,0 +1,2 @@
+from .ml_congestion_map import ExportCongestionMap
+from .__version__ import __version__  # Plugins must expose __version__
---
+++ librelane_plugin_ml/ml_congestion_map.py
@@ -0,0 +1,64 @@
+
+import os
+from typing import Tuple
+
+from librelane.steps import Step, OpenROADStep
+from librelane.config import Variable
+from librelane.steps.step import ViewsUpdate, MetricsUpdate
+from librelane.state.design_format import DesignFormat
+from librelane.steps.common_variables import grt_variables, rsz_variables
+
+@Step.factory.register()
+class ExportCongestionMap(OpenROADStep):
+    """
+    Exports congestion information from a global-placed ODB file.
+    """
+
+    id = "OpenROAD.ExportCongestionMap"
+    name = "Export Congestion Map"
+
+    outputs = [DesignFormat.ODB, DesignFormat.DEF]
+
+    config_vars = (
+        OpenROADStep.config_vars
+        + grt_variables
+        + rsz_variables
+        + [
+            Variable(
+                "DESIGN_REPAIR_BUFFER_INPUT_PORTS",
+                bool,
+                "Specifies whether or not to insert buffers on input ports when design repairs are run.",
+                default=True,
+                deprecated_names=["PL_RESIZER_BUFFER_INPUT_PORTS"],
+            ),
+            Variable(
+                "DESIGN_REPAIR_BUFFER_OUTPUT_PORTS",
+                bool,
+                "Specifies whether or not to insert buffers on output ports when design repairs are run.",
+                default=True,
+                deprecated_names=["PL_RESIZER_BUFFER_OUTPUT_PORTS"],
+            ),
+            Variable(
+                "DESIGN_REPAIR_REMOVE_BUFFERS",
+                bool,
+                "Invokes OpenROAD's remove_buffers command to remove buffers from synthesis, which gives OpenROAD more flexibility when buffering nets.",
+                default=False,
+            ),
+        ]
+    )
+
+    def run(
+        self,
+        state_in,
+        **kwargs,
+    ) -> Tuple[ViewsUpdate, MetricsUpdate]:
+        kwargs, env = self.extract_env(kwargs)
+        return super().run(
+            state_in,
+            corners=self.config["RSZ_CORNERS"] or self.config["STA_CORNERS"],
+            env=env,
+            **kwargs,
+        )
+
+    def get_script_path(self):
+        return os.path.join(os.path.dirname(__file__), "scripts", "export_congestion_map.tcl")
